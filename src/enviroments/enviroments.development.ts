f2c3659b8aa6778ecaca3fdf52e91cf1bd1e855c
EdgardoJorge1 <ehuaricapcha86@protonmail.com>
2026-04-06 21:06:24 -0500

    enviroment

--- src/enviroments/enviroments.development.ts
+++ src/enviroments/enviroments.development.ts
@@ -1,5 +1,6 @@
 export const enviroment = {
     production : false,
-    backenbaseurl : "https://localhost:7200",
+    backenbaseurl : "https://polleriaback-1v6u.onrender.com",
+    //backenbaseurl : "https://localhost:7200",
     apisPeruToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImVodWFyaWNhcGNoYTg2QGdtYWlsLmNvbSJ9.wmsG5j6WqbKzZa3i0jQMSEKggrrjlJbfMve9um_RbLU"
 }
