115d7e668fe0fa0ebac9ddde9eac82f493795c0c
EdgardoJorge1 <ehuaricapcha86@protonmail.com>
2026-06-02 20:34:18 -0500

    web

--- src/enviroments/enviroments.development.ts
+++ src/enviroments/enviroments.development.ts
@@ -1,7 +1,7 @@
 export const enviroment = {
     production : false,
     /* backenbaseurl : "https://polleriaback-1v6u.onrender.com", */
-    backenbaseurl : "https://polleriaback-1.onrender.com",
+    backenbaseurl : "https://polleriaback1.onrender.com",
     apisPeruToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImVodWFyaWNhcGNoYTg2QGdtYWlsLmNvbSJ9.wmsG5j6WqbKzZa3i0jQMSEKggrrjlJbfMve9um_RbLU",
     googleMapsApiKey: "", // Colocar API KEY aquí
     pusher: {
